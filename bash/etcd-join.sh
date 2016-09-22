e885bc04ab47d8dd1d4b66c31324c7e0ea0be4fa
MySQL <871810908@qq.com>
2016-09-22 09:55:01 +0800

    Update etcd-join.sh

--- bash/etcd-join.sh
+++ bash/etcd-join.sh
@@ -4,21 +4,21 @@
 # $4 sb-instanceid-etcd
 # $5 rootpasswd
 
-echo "----->join" $2 "into" $1 "with" $3  "use id " $4
+#echo "----->join" $2 "into" $1 "with" $3  "use id " $4
 
-export ETCDCTL_ENDPOINT=$1
+#export ETCDCTL_ENDPOINT=$1
 
-echo "----->remove $3"
+#echo "----->remove $3"
 #tmpnode=`etcdctl  member list |grep $3|awk -F ':' '{print $1}'|awk -F '[' '{print $1}' `
-echo "remove nodeid:" $tmpnode
-etcdctl  member remove $tmpnode
+#echo "remove nodeid:" $tmpnode
+#etcdctl  member remove $tmpnode
 
-echo "----->add $2 $3"
+#echo "----->add $2 $3"
 #eval `etcdctl  member add  $3 | grep ETCD_INITIAL_CLUSTER`
-export ETCD_INITIAL_CLUSTER_STATE=existing
+#export ETCD_INITIAL_CLUSTER_STATE=existing
 export ETCD_NAME=$2
 
-echo "----->start etcd"
-echo "ETCD_INITIAL_CLUSTER:" $ETCD_INITIAL_CLUSTER
+#echo "----->start etcd"
+#echo "ETCD_INITIAL_CLUSTER:" $ETCD_INITIAL_CLUSTER
 etcd -initial-cluster etcd2=http://etcd-2:2380,etcd1=http://etcd-1:2380,etcd3=http://etcd-3:2380 -initial-cluster-token sb-etcd-test-etcd -initial-advertise-peer-urls $3 -listen-peer-urls http://0.0.0.0:2380 -listen-client-urls http://0.0.0.0:2379 -advertise-client-urls $1 -initial-cluster-state $6 
 
