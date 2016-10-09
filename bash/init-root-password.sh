b277d508bd3ce777d6787b1ff303c2f00fd06c0d
MySQL <871810908@qq.com>
2016-10-09 17:59:08 +0800

    Update init-root-password.sh

--- bash/init-root-password.sh
+++ bash/init-root-password.sh
@@ -9,7 +9,7 @@ do
 			etcdctl --endpoints $2 user add root << EOF
 $1
 EOF
-			etcdctl --endpoints $2 auth enable
+			etcdctl --endpoints $2 -u root:$1 auth enable
 			etcdctl --endpoints $2 -u root:$1 role revoke guest --path '/*' -readwrite
 			etcdctl --endpoints $2 -u root:$1 role add binduser
 			etcdctl --endpoints $2 -u root:$1 role grant binduser -path '/*' -readwrite
