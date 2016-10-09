0bf4548e7dc469a0b5c029fd8c67c017262636e3
MySQL <871810908@qq.com>
2016-10-09 17:31:40 +0800

    Update init-root-password.sh

--- bash/init-root-password.sh
+++ bash/init-root-password.sh
@@ -3,16 +3,16 @@
 #!/bin/bash
 while true
 do
-hh=$(etcdctl --endpoints $2 -u root:$1 cluster-health | tail -1|awk '{print $3}')                        
-if [ h"$hh" = h"healthy" ]
-then              
-etcdctl --endpoints $2 user add root << EOF
+  hh=$(etcdctl --endpoints $2 -u root:$1 cluster-health | tail -1|awk '{print $3}')                        
+  if [ h"$hh" = h"healthy" ]
+    then              
+      etcdctl --endpoints $2 user add root << EOF
 $1                  
 EOF                 
-etcdctl --endpoints $2 auth enable
-etcdctl --endpoints $2 -u root:$1 role revoke guest --path '/*' -readwrite
-etcdctl --endpoints $2 -u root:$1 role add binduser
-etcdctl --endpoints $2 -u root:$1 role grant binduser -path '/*' -readwrite
-exit                                                       
-fi                                                                    
+       etcdctl --endpoints $2 auth enable
+       etcdctl --endpoints $2 -u root:$1 role revoke guest --path '/*' -readwrite
+       etcdctl --endpoints $2 -u root:$1 role add binduser
+       etcdctl --endpoints $2 -u root:$1 role grant binduser -path '/*' -readwrite
+       exit                                                       
+   fi                                                                
 done
